399654fa8419d4a9b45fffc0184a8bbfda4a8e21
alejandropj <alejandroparrajimenez@gmail.com>
2024-04-02 13:46:40 +0200

    Add project files.

---
+++ WCFLogicaPrimerServicio/IMetodosVariosContract.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFLogicaPrimerServicio
+{
+    [ServiceContract]
+    public interface IMetodosVariosContract
+    {
+        [OperationContract]
+        string GetSaludo(string nombre);
+        [OperationContract]
+        int GetNumeroDoble(int numero);
+
+        string MetodoInvisible();
+
+    }
+}
